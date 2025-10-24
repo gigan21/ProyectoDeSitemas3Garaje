9765292091622b8c0ff90513b094a06e74dbbf4d
gigan21 <radarojasc@gmail.com>
2025-10-24 14:01:39 -0400

    Actualizo bd sessionse

--- app/views/layouts/app.blade.php
+++ app/views/layouts/app.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- FORZAR HTTPS PARA RECURSOS INSEGUROS -->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Garaje Alfaro - @yield('title')</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
